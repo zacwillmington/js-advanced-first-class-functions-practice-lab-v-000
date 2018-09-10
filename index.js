8702dc8d99e685c893e2d30871309df510659523
Zac Willmington <zac.willmington@gmail.com>
2018-09-10 18:31:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+
+function logDriverNames (drivers){
+    drivers.forEach(driver => console.log(driver.name));
+}
+
+function logDriversByHometown (drivers, hometown){
+    const driversbyHomeTown = drivers.filter(driver => driver.hometown === hometown);
+
+    logDriverNames(driversbyHomeTown);
+}
+
+function driversByRevenue (drivers){
+    const lowToHigh = drivers.slice().sort((a,b) => a.revenue - b.revenue);
+    return lowToHigh;
+
+}
