7f911c696a173ae35c68ed2504d5b9ecdbfb8aed
Zac Willmington <zac.willmington@gmail.com>
2018-09-10 19:01:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+
+function logDriverNames (drivers){
+    drivers.forEach(driver => console.log(driver.name));
+}
+
+function logDriversByHometown (drivers, hometown){
+    const driversbyHomeTown = drivers.filter(driver => driver.hometown === hometown);
+
+    logDriverNames(driversbyHomeTown);
+}
+
+function driversByRevenue (drivers){
+    const lowToHigh = drivers.slice().sort((a,b) => a.revenue - b.revenue);
+    return lowToHigh;
+
+}
+
+function driversByName (drivers){
+    const sortedByName = drivers.slice().sort((a,b) => a.name.localeCompare(b.name));
+
+    return sortedByName;
+}
+
+function totalRevenue (drivers){
+    const revenueArray = drivers.map(driver => driver.revenue);
+
+    const sum = revenueArray.reduce((total, amount) =>  total + amount);
+    return sum;
+}
+
+function averageRevenue (drivers){
+
+    return totalRevenue(drivers) / 5;
+
+}
